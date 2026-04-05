5968cb6b03fc7c3ff25fbf2f09e424c4a623f655
Patch Test <patch-test@local>
2026-04-05 16:34:13 -0600

    fix(color-palettes): enable gradient rendering for shape palettes

--- blocksuite/affine/model/src/elements/shape/shape.ts
+++ blocksuite/affine/model/src/elements/shape/shape.ts
@@ -40,12 +40,23 @@ export type ShapeProps = BaseElementProps & {
   fillColor: Color;
   gradientFinal?: Color;
   gradientDirection?: GradientDirection;
+  flipX?: boolean;
+  flipY?: boolean;
+  lockAspectRatio?: boolean;
+  textRotate?: number;
+  textFlipX?: boolean;
+  textFlipY?: boolean;
   strokeWidth: number;
   strokeColor: Color;
   strokeStyle: StrokeStyle;
   shapeStyle: ShapeStyle;
   // https://github.com/rough-stuff/rough/wiki#roughness
   roughness?: number;
+  stencilName?: string;
+  collapsed?: boolean;
+  collapsedSize?: [number, number];
+  expandedSize?: [number, number];
+  collapseProxyId?: string | null;
 
   text?: Y.Text;
   textHorizontalAlign?: TextAlign;
@@ -111,6 +122,24 @@ export class ShapeElementModel extends GfxPrimitiveElementModel<ShapeProps> {
   accessor gradientDirection: GradientDirection | undefined = undefined;
 
   @field()
+  accessor flipX: boolean = false;
+
+  @field()
+  accessor flipY: boolean = false;
+
+  @field()
+  accessor lockAspectRatio: boolean = false;
+
+  @field()
+  accessor textRotate: number = 0;
+
+  @field()
+  accessor textFlipX: boolean = false;
+
+  @field()
+  accessor textFlipY: boolean = false;
+
+  @field()
   accessor filled: boolean = false;
 
   @field(FontFamily.Inter as string)
@@ -164,6 +193,9 @@ export class ShapeElementModel extends GfxPrimitiveElementModel<ShapeProps> {
   accessor shapeType: ShapeType = ShapeType.Rect;
 
   @field()
+  accessor stencilName: string | undefined = undefined;
+
+  @field()
   accessor strokeColor: Color = DefaultTheme.shapeStrokeColor;
 
   @field()
@@ -171,6 +203,15 @@ export class ShapeElementModel extends GfxPrimitiveElementModel<ShapeProps> {
 
   @field()
   accessor strokeWidth: number = 4;
+
+  @field(false)
+  accessor collapsed: boolean = false;
+
+  @field()
+  accessor collapsedSize: [number, number] | undefined = undefined;
+
+  @field()
+  accessor expandedSize: [number, number] | undefined = undefined;
 
   @field()
   accessor text: Y.Text | undefined = undefined;
@@ -181,11 +222,14 @@ export class ShapeElementModel extends GfxPrimitiveElementModel<ShapeProps> {
   @local()
   accessor textDisplay: boolean = true;
 
+  @field()
+  accessor collapseProxyId: string | null = null;
+
   @field(TextAlign.Center as TextAlign)
   accessor textHorizontalAlign!: TextAlign;
 
-  @field(TextResizing.AUTO_HEIGHT as TextResizing)
-  accessor textResizing: TextResizing = TextResizing.AUTO_HEIGHT;
+  @field(TextResizing.NONE as TextResizing)
+  accessor textResizing: TextResizing = TextResizing.NONE;
 
   @field(TextVerticalAlign.Center as TextVerticalAlign)
   accessor textVerticalAlign!: TextVerticalAlign;
@@ -257,6 +301,18 @@ export class LocalShapeElementModel extends GfxLocalElementModel {
 
   @prop()
   accessor strokeWidth: number = 4;
+
+  @prop()
+  accessor lockAspectRatio: boolean = false;
+
+  @prop()
+  accessor textRotate: number = 0;
+
+  @prop()
+  accessor textFlipX: boolean = false;
+
+  @prop()
+  accessor textFlipY: boolean = false;
 
   @prop()
   accessor text: string = '';
