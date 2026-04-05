a51a08af2958dcebef5751cd930d88e681307bc7
Patch Test <patch-test@local>
2026-04-04 21:28:41 -0600

    feat(dotted-line-style): add connector editor border style parity

--- packages/frontend/core/src/desktop/dialogs/setting/general-setting/editor/edgeless/connector.tsx
+++ packages/frontend/core/src/desktop/dialogs/setting/general-setting/editor/edgeless/connector.tsx
@@ -127,13 +127,25 @@ export const ConnectorSettings = () => {
     () => [
       {
         value: StrokeStyle.Solid,
+        testId: 'connector-border-style-solid-trigger',
         label:
           t['com.affine.settings.editorSettings.edgeless.note.border.solid'](),
       },
       {
         value: StrokeStyle.Dash,
+        testId: 'connector-border-style-dash-trigger',
         label:
           t['com.affine.settings.editorSettings.edgeless.note.border.dash'](),
+      },
+      {
+        value: StrokeStyle.Dot,
+        testId: 'connector-border-style-dot-trigger',
+        label: 'Dotted',
+      },
+      {
+        value: StrokeStyle.None,
+        testId: 'connector-border-style-none-trigger',
+        label: 'None',
       },
     ],
     [t]
@@ -443,6 +455,7 @@ export const ConnectorSettings = () => {
           max={12}
           step={2}
           nodes={[2, 4, 6, 8, 10, 12]}
+          disabled={borderStyle === StrokeStyle.None}
         />
       </SettingRow>
       <SettingRow
