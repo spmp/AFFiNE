90594cc70f2519cf07433dc11f7253fded4f4ba7
Patch Test <patch-test@local>
2026-04-25 15:34:45 -0600

    Flip and rotate is OK, arrow endpoint direction still problematic, as is 'next shape' arrow placement for some rotation angles

--- blocksuite/affine/blocks/root/src/edgeless/configs/toolbar/more.ts
+++ blocksuite/affine/blocks/root/src/edgeless/configs/toolbar/more.ts
@@ -32,6 +32,7 @@ import type {
   ToolbarActions,
   ToolbarContext,
 } from '@blocksuite/affine-shared/services';
+import type { ConnectorElementModel } from '@blocksuite/affine-model';
 import {
   matchModels,
   type ReorderingType,
