4fd6e4e500170ff111f094e069e4b1e7107b23a8
Patch Test <patch-test@local>
2026-04-26 18:47:01 -0600

    Better arrow angle and OK next shape angle is OK

--- blocksuite/affine/widgets/edgeless-selected-rect/src/edgeless-auto-complete.ts
+++ blocksuite/affine/widgets/edgeless-selected-rect/src/edgeless-auto-complete.ts
@@ -11,6 +11,7 @@ import {
 import {
   type ConnectionOverlay,
   ConnectorPathGenerator,
+  getConnectableRelativePosition,
 } from '@blocksuite/affine-gfx-connector';
 import { mountShapeTextEditor } from '@blocksuite/affine-gfx-shape';
 import type {
