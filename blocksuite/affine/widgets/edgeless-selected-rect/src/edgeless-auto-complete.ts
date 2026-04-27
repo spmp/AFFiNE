ef788882b1649fc3478cf9391026ae9057101a1d
Patch Test <patch-test@local>
2026-04-27 13:37:03 -0600

    Final fixes for flip menu and next shape

--- blocksuite/affine/widgets/edgeless-selected-rect/src/edgeless-auto-complete.ts
+++ blocksuite/affine/widgets/edgeless-selected-rect/src/edgeless-auto-complete.ts
@@ -11,7 +11,6 @@ import {
 import {
   type ConnectionOverlay,
   ConnectorPathGenerator,
-  getConnectableRelativePosition,
 } from '@blocksuite/affine-gfx-connector';
 import { mountShapeTextEditor } from '@blocksuite/affine-gfx-shape';
 import type {
@@ -339,13 +338,11 @@ export class EdgelessAutoComplete extends WithDisposable(LitElement) {
       endPosition,
       nextShape
     );
-    const startPoint = getConnectableRelativePosition(
-      curShape,
-      startConnectionPosition
+    const startPoint = PointLocation.fromVec(
+      startBound.getRelativePoint(startConnectionPosition)
     );
-    const endPoint = getConnectableRelativePosition(
-      nextShape,
-      endConnectionPosition
+    const endPoint = PointLocation.fromVec(
+      nextBound.getRelativePoint(endConnectionPosition)
     );
 
     return this._pathGenerator.generateOrthogonalConnectorPath({
