990b4d0637e890a0343420a55db363cfebaf22ca
Patch Test <patch-test@local>
2026-05-19 18:14:26 -0600

    10: Advanced shapes, start again after refactor

--- blocksuite/affine/model/src/elements/shape/shape.ts
+++ blocksuite/affine/model/src/elements/shape/shape.ts
@@ -309,12 +309,6 @@ export class LocalShapeElementModel extends GfxLocalElementModel {
   accessor fillColor: Color = DefaultTheme.shapeFillColor;
 
   @prop()
-  accessor flipX: boolean = false;
-
-  @prop()
-  accessor flipY: boolean = false;
-
-  @prop()
   accessor filled: boolean = false;
 
   @prop()
